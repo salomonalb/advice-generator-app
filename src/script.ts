5ee2cd8a483291b052922bf7eaaeb854e8eda194
salomonalb <95834451+salomonalb@users.noreply.github.com>
2022-11-17 00:53:34 -0400

    get the elements

---
+++ src/script.ts
@@ -0,0 +1,4 @@
+// Get HTML ELements
+const adviceIdEl = document.querySelector("#advice-id") as HTMLTitleElement;
+const adviceEl = document.querySelector("#advice") as HTMLParagraphElement;
+const buttonEl = document.querySelector("#button") as HTMLButtonElement;
